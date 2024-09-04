f94c31f1b09eb46fe3d10a98cd1301154e1259bb
Mishan Parajuli <mparajuli1@stu.jsu.edu>
2024-09-05 01:40:59 +0545

    Create Matrix_mux.java

---
+++ Matrix_mux.java
@@ -0,0 +1,36 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] result = new int[3][3];
+
+        // Multiplying two matrices
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        // Displaying the result
+        System.out.println("Result of Matrix Multiplication:");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
